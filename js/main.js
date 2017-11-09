f4bb8877e164295723a8bc727207c1af82cfda42
Kayla Ervin <kayla.ervin@gmail.com>
2017-11-08 21:21:46 -0800

    all files

---
+++ js/main.js
@@ -0,0 +1,17 @@
+//instantiate clipboard.js
+$( document ).ready(function() {
+    var clipboard = new Clipboard('.clipboard');
+  });
+
+//on footer button click, add class to show copied to clipboard message
+$(".clipboard").click(function(){
+  $(".clip-message").addClass("show-hide");
+});
+
+//after copied to clipboard message animates, remove class to hide
+$(".clip-message").on(
+  "webkitAnimationEnd oanimationend msAnimationEnd animationend",
+  function() {
+      $(this).removeClass("show-hide");
+  }
+);
